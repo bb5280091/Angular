ac646cfdf2a93a1ddd51f8a7e8141f6d35477c88
cfh01 <cfh01@cathayholdings.com.tw>
2023-10-31 15:08:12 +0800

    10/31下午更新

--- src/app/components/member/apply-record-page/apply-record-page.component.ts
+++ src/app/components/member/apply-record-page/apply-record-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MinimalistAnimal } from 'src/app/adpotion-model';
 import { AdoptionService } from 'src/app/service/adoption.service';
 
@@ -7,18 +8,31 @@ import { AdoptionService } from 'src/app/service/adoption.service';
   templateUrl: './apply-record-page.component.html',
   styleUrls: ['./apply-record-page.component.css']
 })
-export class ApplyRecordPageComponent implements OnInit{
-  constructor(private adoptionService: AdoptionService){}
-  dispalyAnimal! :MinimalistAnimal[];
+export class ApplyRecordPageComponent implements OnInit {
+  constructor(private adoptionService: AdoptionService,private router: Router) { }
+  dispalyAnimal!: MinimalistAnimal[];
   ngOnInit(): void {
-   this.adoptionService.onGetUserSubscription(Number(localStorage.getItem('userId'))).subscribe((res)=>{
-   if('response' in res){
-    this.dispalyAnimal=res.response;
-   }else{
-    console.log('查無資料')
-   }
+    this.adoptionService.onGetUserSubscription(Number(localStorage.getItem('userId'))).subscribe((res) => {
+      if ('response' in res) {
+        this.dispalyAnimal = res.response;
+      } else {
+        console.log('查無資料')
+      }
 
-   })
+    })
+  }
+  unsubscription(id: number) {
+    this.adoptionService.UserUnsubscription(Number(localStorage.getItem('userId')), id).subscribe((res) => {
+      if (res.statusCode === '0000') {
+        console.log('刪除成功');
+      } else {
+        console.log('刪除失敗');
+      }
+
+    })
+  }
+  navigateToYourPath(id :number) {
+    this.router.navigate(['/detail'], { queryParams: { animalId: id } });
   }
 
 }
